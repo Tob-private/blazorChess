8597a323cc738c849ab1abdf850dcc5b22261082
Tobias Riiser <tobias.riiser.games@gmail.com>
2024-08-13 13:02:42 +0200

    supporting FEN strings now

--- Components/Pages/Logic/Board.cs
+++ Components/Pages/Logic/Board.cs
@@ -1,14 +1,49 @@
-namespace blazorChess.Components.Pages.Logic {
+using Microsoft.VisualBasic;
 
-public class BoardClass
+namespace blazorChess.Components.Pages.Logic
 {
-public static readonly int[] Squares = new int[64];
 
-    public static void Board() {
+    public class BoardClass
+    {
+        public static readonly int[] Squares = new int[64];
+        public const string startingFEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+        public static void Board()
+        {
+            LoadPositionFromFen(startingFEN);
+        }
 
-        Squares[0] = Piece.White | Piece.Bishop;
-        Squares[32] = Piece.White | Piece.King;
-        Squares[61] = Piece.Black | Piece.Queen;
+        public static void LoadPositionFromFen(string fen)
+        {
+            var PieceFromFen = new Dictionary<char, int>()
+            {
+                ['k'] = Piece.King, 
+                ['p'] = Piece.Pawn, 
+                ['n'] = Piece.Knight, 
+                ['b'] = Piece.Bishop, 
+                ['r'] = Piece.Rook, 
+                ['q'] = Piece.Queen
+            };
+            
+            string fenBoard = fen.Split(' ')[0];
+            int file = 0, rank = 0;
+
+            foreach (char symbol in fenBoard)
+            {
+                if(symbol == '/') {
+                    file = 0;
+                    rank++;
+                } else {
+                    if (char.IsDigit(symbol))
+                    {
+                        file += (int) char.GetNumericValue(symbol);
+                    } else {
+                        int pieceColor = char.IsUpper(symbol) ? Piece.White : Piece.Black;
+                        int pieceType = PieceFromFen[char.ToLower(symbol)];
+                        Squares[rank * 8 + file] = pieceType | pieceColor;
+                        file++;
+                    }
+                }
+            }
+        }
     }
-}
 }
