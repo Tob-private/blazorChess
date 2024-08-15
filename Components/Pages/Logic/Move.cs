605611259b52904d2150db800886d5d3c8810d1c
Tobias Riiser <tobias.riiser.games@gmail.com>
2024-08-15 16:09:36 +0200

    mr gpt cleaned up my code a bit( and by a bit, I mean a lot)

--- Components/Pages/Logic/Move.cs
+++ Components/Pages/Logic/Move.cs
@@ -3,75 +3,38 @@ namespace blazorChess.Components.Pages.Logic
     public static class Move
     {
         public static void DisplayMoves(int pieceInBinary, int squareIndex)
-        {
-            Console.WriteLine("----------------------------------------------------------");
-            int pieceType;
-            string pieceString = pieceInBinary.ToString();
-            List<int>[] possibleMoves = new List<int>[27];
-            if (pieceString.Length == 4) // White piece
-            {
-                pieceType = int.Parse(pieceString[1..]);
+{
+    Console.WriteLine("----------------------------------------------------------");
+    string pieceString = pieceInBinary.ToString();
+    bool isWhite = pieceString.Length == 4;
+    int pieceType = isWhite ? int.Parse(pieceString[1..]) : int.Parse(pieceString[2..]);
 
-                if (pieceType == int.Parse(Convert.ToString(Piece.Pawn, 2)))
-                {
-                    Console.WriteLine("White Pawn press");
-                }
-                else if (pieceType == int.Parse(Convert.ToString(Piece.Queen, 2)))
-                {
-                    Console.WriteLine("White Queen press");
-                }
-                else if (pieceType == int.Parse(Convert.ToString(Piece.King, 2)))
-                {
-                    Console.WriteLine("White King press");
-                }
-                else if (pieceType == int.Parse(Convert.ToString(Piece.Knight, 2)))
-                {
-                    Console.WriteLine("White Knight press");
-                }
-                else if (pieceType == int.Parse(Convert.ToString(Piece.Rook, 2)))
-                {
-                    Console.WriteLine("White Rook press");
-                }
-                else if (pieceType == int.Parse(Convert.ToString(Piece.Bishop, 2)))
-                {
-                    Console.WriteLine("White Bishop press");
-                }
+    string pieceColor = isWhite ? "White" : "Black";
+    string pieceName = GetPieceName(pieceType);
 
 
-            }
-            if (pieceString.Length == 5) // Black piece
-            {
-                pieceType = int.Parse(pieceString[2..]);
+    if (pieceName != null)
+    {
+        Console.WriteLine($"{pieceColor} {pieceName} press");
+    }
 
-                if (pieceType == int.Parse(Convert.ToString(Piece.Pawn, 2)))
-                {
-                    Console.WriteLine("Black Pawn press");
-                }
-                else if (pieceType == int.Parse(Convert.ToString(Piece.Queen, 2)))
-                {
-                    Console.WriteLine("Black Queen press");
-                }
-                else if (pieceType == int.Parse(Convert.ToString(Piece.King, 2)))
-                {
-                    Console.WriteLine("Black King press");
-                }
-                else if (pieceType == int.Parse(Convert.ToString(Piece.Knight, 2)))
-                {
-                    Console.WriteLine("Black Knight press");
-                }
-                else if (pieceType == int.Parse(Convert.ToString(Piece.Rook, 2)))
-                {
-                    Console.WriteLine("Black Rook press");
-                }
-                else if (pieceType == int.Parse(Convert.ToString(Piece.Bishop, 2)))
-                {
-                    Console.WriteLine("Black Bishop press");
-                }
-            }
+    Console.WriteLine("square index: " + squareIndex);
+}
 
-            Console.WriteLine("square index: " + squareIndex);
+private static string GetPieceName(int pieceType)
+{
+    return pieceType switch
+    {
+        int type when type == int.Parse(Convert.ToString(Piece.Pawn, 2)) => "Pawn",
+        int type when type == int.Parse(Convert.ToString(Piece.Queen, 2)) => "Queen",
+        int type when type == int.Parse(Convert.ToString(Piece.King, 2)) => "King",
+        int type when type == int.Parse(Convert.ToString(Piece.Knight, 2)) => "Knight",
+        int type when type == int.Parse(Convert.ToString(Piece.Rook, 2)) => "Rook",
+        int type when type == int.Parse(Convert.ToString(Piece.Bishop, 2)) => "Bishop",
+        _ => "Unknown Piece name"
+    };
+}
 
-        }
 
         public static void MovePiece(List<int> squaresArray, int currentIndex, int newIndex)
         {
